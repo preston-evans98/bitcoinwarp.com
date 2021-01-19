5865d8c0b958b3eb1b1a1a6dae3640218b66cdfa
Preston Evans <pbevans1@crimson.ua.edu>
2021-01-19 10:27:56 -0600

    update docs

---
+++ doc/implementors/core/hash/trait.Hash.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["shared"] = [{"text":"impl Hash for u256","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
